b152aeddd9d44d58225f4c967114be90b7512afb
James Bowman <jamesb@excamera.com>
2017-09-19 11:43:54 -0700

    roll and pick

--- core.fs
+++ core.fs
@@ -40,3 +40,20 @@
     then
     drop ;
 
+: roll
+    ?dup if
+        swap >r
+        1- recurse
+        r> swap
+    then
+;
+
+: pick
+    ?dup if
+        swap >r
+        1- recurse
+        r> swap
+    else
+        dup
+    then
+;
